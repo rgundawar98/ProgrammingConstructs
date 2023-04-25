f28821012bfdda4d473c26eb1c81bab6a70e17b6
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-04-25 16:18:44 +0530

    [Rahul]Adding welcome message

---
+++ ProgrammingConstructs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgrammingConstructs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Programming Constructs");
+        }
+    }
+}
